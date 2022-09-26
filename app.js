1e8f71f498a3dc4dbd4a4aa451094fba64fc4e2e
Nelson teixeira <nelsontxeira@gmail.com>
2022-09-26 19:23:35 -0300

    refactory

--- app.js
+++ app.js
@@ -25,17 +25,4 @@ app.listen(PORT, () => {
   console.log(`Running in http://localhost:${PORT}`);
 })
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   res.status(err.status || 500);
-// });
-
 module.exports = app;
